b44aec6d9eb04cfac890949cfe03043cc44443d6
KinXpeng <kinxpeng@163.com>
2022-04-10 18:18:49 +0800

    add http

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,56 +1,61 @@
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import styles from './index.module.scss';
 
 function Login() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
+    navigate('/', { replace: true });
   };
   return (
-    <Form
-      name="normal_login"
-      className={styles['login-form']}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: t('login.username_check'),
-          },
-        ]}
+    <div className={styles['login_panel']}>
+      <Form
+        name="normal_login"
+        className={styles['login-form']}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder={t('login.username')} />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: t('login.password_check'),
-          },
-        ]}
-      >
-        <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder={t('login.password')} />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>{t('login.remember')}</Checkbox>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: t('login.username_check'),
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder={t('login.username')} />
         </Form.Item>
-      </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: t('login.password_check'),
+            },
+          ]}
+        >
+          <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder={t('login.password')} />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>{t('login.remember')}</Checkbox>
+          </Form.Item>
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className={styles['login-form-button']}>
-          {t('login.login')}
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className={styles['login-form-button']}>
+            {t('login.login')}
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
 export default Login;
