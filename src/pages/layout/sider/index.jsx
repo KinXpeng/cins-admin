1abb9ecdf96cb80930ec352e6ad4d3b569081e99
KinXpeng <kinxpeng@163.com>
2022-04-12 23:53:29 +0800

    点击logo回到首页

--- src/pages/layout/sider/index.jsx
+++ src/pages/layout/sider/index.jsx
@@ -3,7 +3,7 @@ import { useStore } from '@/store/index';
 import { observer } from 'mobx-react-lite';
 import { Menu } from 'antd';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { TeamOutlined, TableOutlined, SmileOutlined } from '@ant-design/icons';
 import styles from './index.module.scss';
 
@@ -12,17 +12,26 @@ const { SubMenu } = Menu;
 function SiderMenu({ collapsed }) {
   const { configStore } = useStore();
   const { t } = useTranslation();
+  const navigate = useNavigate(); // 路由跳转
 
   // 解决刷新页面面包屑导航消失的问题
   useEffect(() => {
     let key = localStorage.getItem('activeItem');
-    if (key !== undefined) {
+    if (key !== undefined && key !== null) {
       configStore.switchMenuItem({ key: key });
     }
   }, [configStore]);
+
+  // 返回首页
+  const backHome = () => {
+    configStore.crumbItem();
+    navigate('/', { replace: true });
+  };
   return (
     <>
-      <div className={styles.logo}>{collapsed ? 'L' : 'logo'}</div>
+      <div className={styles.logo} onClick={backHome}>
+        {collapsed ? 'L' : 'logo'}
+      </div>
       <Menu theme={configStore.themeStyle} mode="inline" selectedKeys={[configStore.activeItem]} onClick={configStore.switchMenuItem}>
         <SubMenu key="user" icon={<TeamOutlined />} title={t('aside.user_list')}>
           <Menu.Item key="Option 1">
